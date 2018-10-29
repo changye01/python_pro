ed43e24029e41630644977fbb74cbb66d36f80a9
changye <1421698291@qq.com>
2018-10-29 14:32:45 +0800

    testing

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+a = 100
+if a>100:
+    print(a)
+else:
+    print(-a)
+print(3<5)
+print(True or False)
+print(not True)
+a=b=c=1
+z,x,y=1,2,3
+# print a,b,c,x,z,y
+print(a,b,c,z,x,y)
+if a<z:
+    print('1')
+elif x<y:
+    print('2')
+else:
+    print('3')
+a = 5
+b = 3
+# /	    除 - x 除以 y
+# //	取整除 - 返回商的整数部分 
+print (a/b)
+# while(1):
+#     print('hello world')
+
+var1 = 'hello world'
+var2 = 'changye'
+# print(var1[0])
+# 1:5 从1到5的前一个字符串
+# print(var2[1:5])
+print(var1[0:6] + 'Runoob!')
+list = ['a', 'b', 1]
+print(list)
