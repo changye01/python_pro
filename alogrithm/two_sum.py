bc9b4db36e6ba9563a2005d090e9b906f17f8bb1
changye <1421698291@qq.com>
2018-11-18 00:21:48 +0800

    two sum

---
+++ alogrithm/two_sum.py
@@ -0,0 +1,47 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        ## 1 break
+        # for x in nums:
+        #     print(x) 
+
+
+        #2   break
+        # for i in range(len(nums)):
+        #     flag = nums[i]
+        #     print(flag)x
+        #     nums.pop(0)
+            
+
+
+        #3 
+
+        # success 
+
+        # dict = {}
+        # for i in range(len(nums)):
+        #     if nums[i] in dict:
+        #         return [dict[nums[i]], i]
+        #     else:
+        #         dict[target-nums[i]] = i
+
+
+        # 4 暴力
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == target-nums[j]:
+                    return [i,j]
+                
+
+
+            
+
+nums = [2, 7, 11, 15]
+so = Solution()
+s = so.twoSum(nums,9)
+print(s)
