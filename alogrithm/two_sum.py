314aaade9df7fd18e5eee06a4eac88af6080c3f3
changye <1421698291@qq.com>
2019-04-06 13:58:15 +0800

    manjaro-changye 测试提交

--- alogrithm/two_sum.py
+++ alogrithm/two_sum.py
@@ -1,28 +1,24 @@
 class Solution:
-    def twoSum(self, nums, target):
+    @staticmethod
+    def two_sum(nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
 
-        ## 1 break
+        # 1 break
         # for x in nums:
-        #     print(x) 
+        #     print(x)
 
-
-        #2   break
+        # 2 break
         # for i in range(len(nums)):
         #     flag = nums[i]
         #     print(flag)x
         #     nums.pop(0)
-            
 
-
-        #3 
-
-        # success 
-
+        # 3
+        # success
         # dict = {}
         # for i in range(len(nums)):
         #     if nums[i] in dict:
@@ -30,18 +26,14 @@ class Solution:
         #     else:
         #         dict[target-nums[i]] = i
 
-
         # 4 暴力
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i+1, len(nums)):
                 if nums[i] == target-nums[j]:
-                    return [i,j]
-                
+                    return [i, j]
 
-
-            
 
 nums = [2, 7, 11, 15]
 so = Solution()
-s = so.twoSum(nums,9)
+s = so.two_sum(nums, 9)
 print(s)
